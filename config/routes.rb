9ca5f6632efbfd72ad012d5c393a55230d3dd1d5
wlcreate <waverley.nyc@gmail.com>
2020-10-19 19:42:50 -0400

    fixes keep_logged_in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :recommendations
-  resources :spots
-  resources :neighborhoods
-  resources :categories
-  resources :users
+  # resources :recommendations
+  # resources :spots
+  # resources :neighborhoods
+  # resources :categories
+  # resources :users
 
+  get "/users/keep_logged_in", to: "users#keep_logged_in"
   post "/users/login", to: "users#login"
   post "/users", to: "users#create"
-  get "/users/keep_logged_in", to: "users#keep_logged_in"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
